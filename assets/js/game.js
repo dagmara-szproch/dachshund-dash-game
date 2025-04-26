d7d9ba85430d2844c57305de9ec300e2067de3da
dagmara-szproch <dagmamara@gmail.com>
2025-04-26 11:21:01 +0100

    Add structure to game.js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+// Core Game Variables
+const board = document.getElementById('game-board');
+let dachshund = [{x: 10, y: 10}];
+let food = {x: 5, y: 5};
+let direction = 'RIGHT';
+
+//1. Initialise Board 
+function createGameBoard()
+
+// 2. Main Game Functions
+function moveSnake() 
+
+function checkCollision() 
+
+function generateFood() 
+
+// 3. Keyboard Controls - eventListener
+
+// 4. Game Loop
+function gameLoop() 
