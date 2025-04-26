ae0cc2ac04b08a9723d41419cb20d106544d806f
dagmara-szproch <dagmamara@gmail.com>
2025-04-26 12:17:51 +0100

    Add createGameBoard() to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,38 @@
+console.log('Hello');
+
 // Core Game Variables
+const gridSize = 20;
 const board = document.getElementById('game-board');
 let dachshund = [{x: 10, y: 10}];
 let food = {x: 5, y: 5};
 let direction = 'RIGHT';
 
 //1. Initialise Board 
-function createGameBoard()
+function createGameBoard() {
+    board.innerHTML= ''; //clear previous content
+    board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-// 2. Main Game Functions
-function moveSnake() 
+    for (let i =0; i < gridSize * gridSize; i++) {
+        const cell = document.createElement('div');
+        board.appendChild(cell);
+    }
 
-function checkCollision() 
+}
 
-function generateFood() 
+// // 2. Main Game Functions
+// function moveSnake() 
 
-// 3. Keyboard Controls - eventListener
+// function checkCollision() 
 
-// 4. Game Loop
-function gameLoop() 
+// function generateFood() 
+
+// // 3. Keyboard Controls - eventListener
+
+// // 4. Game Loop
+// function gameLoop() 
+
+
+// Export for testing (bottom of file)
+window.createGameBoard = createGameBoard;
+window.onload = createGameBoard;
